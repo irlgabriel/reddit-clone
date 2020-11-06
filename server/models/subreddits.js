3053913ddcf468ef7f43fbb0c2557c1f3b032930
irlgabriel <radu.gabriel131@gmail.com>
2020-11-06 14:38:35 +0200

    added server side

---
+++ server/models/subreddits.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var SubredditSchema = mongoose.Schema({
+  name: {type: String, unique: true, required: [true, "can't be blank"]},
+  posts: Array,
+}, {timestamps: true})
+var Subreddit = mongoose.model('Subreddit', SubredditSchema);
+module.exports = Subreddit;
---
+++ server/models/users.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  username: {type: String, unique: true, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+  email: {type: String, unique: true, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  password: {type: String, required: [true, "can't be blank"]}
+}, {timestamps: true})
+var User = mongoose.model("User", UserSchema);
+module.exports = User;
