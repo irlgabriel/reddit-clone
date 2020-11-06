dee8f969ab5def86109215f3802ac107ff883b86
irlgabriel <radu.gabriel131@gmail.com>
2020-11-06 15:20:29 +0200

    finished post content

--- client/src/Components/Post/Post.components.js
+++ client/src/Components/Post/Post.components.js
@@ -4,6 +4,9 @@ import {
   GoArrowUp
 } from "react-icons/go";
 import { BsDot } from "react-icons/bs";
+import { RiMessageFill, RiShareForwardFill } from "react-icons/ri";
+import { FaSave } from "react-icons/fa";
+
 
 export const PostContainer = styled.div`
   background-color: white;
@@ -44,11 +47,21 @@ export const DownDot = styled(GoArrowDown)`
   color: gray;
   font-size: 24px;
 `
-export const Topbar = styled.div`
+export const PostContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  //height: 100%; // No idea why this does not work so I will use absolute position instead
+  position: absolute;
+  left: 40px;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  padding: .5rem;
+
+`
+export const PostHeader = styled.div`
   display: flex;
   align-items: center;
-  margin-left: 40px;
-  padding: .25rem;
 `
 export const SubredditName = styled.p`
   font-size: 14px;
@@ -67,4 +80,42 @@ export const MiddleDot = styled(BsDot)`
 `
 export const CreatedAt = styled.p`
   
+`
+
+export const PostBody = styled.div`
+  flex: 1;
+`
+export const PostTitle = styled.p`
+  font-size: 24px;
+`
+export const PostContent = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 300;
+  font-size: 14px;
+`
+export const PostFooter = styled.div`
+  display: flex;
+  margin-top: auto;
+`
+export const FooterLink = styled.a`
+  padding: .25rem;
+  color: darkgray;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-size: 14px;
+  &:hover {
+    background: lightgray;
+    text-decoration: none;
+  }
+  
+`
+export const CommentIcon = styled(RiMessageFill)`
+
+`
+export const ShareIcon = styled(RiShareForwardFill)`
+
+`
+export const SaveIcon = styled(FaSave)`
+
 `
--- client/src/Components/Post/Post.js
+++ client/src/Components/Post/Post.js
@@ -6,10 +6,19 @@ import {
   DotsCount,
   UpDot,
   DownDot,
-  Topbar,
+  PostHeader,
   SubredditName,
   Creator,
-  MiddleDot
+  PostContentWrapper,
+  PostBody,
+  PostFooter,
+  CreatedAt,
+  PostTitle,
+  PostContent,
+  FooterLink,
+  CommentIcon,
+  ShareIcon,
+  SaveIcon
 } from "./Post.components"
 const Post = ({votes, comments, title, subreddit, user, content}) => {
   const [dots, setDots] = useState(0);
@@ -23,10 +32,30 @@ const Post = ({votes, comments, title, subreddit, user, content}) => {
           <DownDot />
         </DotsContainer>
       </DotsWrapper>
-      <Topbar>
-        <SubredditName>r/{subreddit}&nbsp;&middot;&nbsp;</SubredditName>
-        <Creator>{user}</Creator>
-      </Topbar>
+      <PostContentWrapper>
+        <PostHeader>
+          <SubredditName>r/{subreddit}&nbsp;&middot;&nbsp;</SubredditName>
+          <Creator>{user}</Creator>
+        </PostHeader>
+        <PostBody>
+          <PostTitle>{title}</PostTitle>
+          <PostContent>{content}</PostContent>
+        </PostBody>
+        <PostFooter>
+          <FooterLink href="">
+            <CommentIcon />&nbsp;
+            <span>{comments.length} Comments</span>
+          </FooterLink>
+          <FooterLink href="">
+            <ShareIcon />&nbsp;
+            <span>Share</span>
+          </FooterLink>
+          <FooterLink href="">
+            <SaveIcon />&nbsp;
+            <span>Save</span>
+          </FooterLink>
+        </PostFooter>
+      </PostContentWrapper>
     </PostContainer>  
   )
 }
