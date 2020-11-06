fcaec26622da856f902b2cf703e7fa5d1d26823f
irlgabriel <radu.gabriel131@gmail.com>
2020-11-06 02:27:35 +0200

    restructured client/server paths

--- api/models/subreddits.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var SubredditSchema = mongoose.Schema({
-  name: {type: String, unique: true, required: [true, "can't be blank"]},
-  posts: Array,
-})
-var Subreddit = mongoose.model('Subreddit', SubredditSchema);
-module.exports = Subreddit;
--- api/models/users.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
-  username: {type: String, unique: true, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-  email: {type: String, unique: true, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-  password: {type: String, required: [true, "can't be blank"]}
-})
-var User = mongoose.model("User", UserSchema);
-module.exports = User;
