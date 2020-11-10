477bf0ea5d64f025c4b588abd0efcc8daf61ad77
irlgabriel <radu.gabriel131@gmail.com>
2020-11-10 19:33:24 +0200

    added comment body and header - still need to figure the layout a bit

--- client/src/Components/PostComments/PostComment.components.js
+++ client/src/Components/PostComments/PostComment.components.js
@@ -2,12 +2,18 @@ import styled from "styled-components";
 import { GoArrowDown, GoArrowUp } from "react-icons/go";
 
 export const CommentWrapper = styled.div`
+
 `
 export const DotsGroup = styled.div`
-  display: inline-block;
+  display: inline-flex;
+  flex-direction: column;
 `
 export const CommentContainer = styled.div`
 
+`
+export const CommentContent = styled.div`
+  display: inline-block;
+  padding: 0;
 `
 export const Username = styled.span`
   font-size: 11px;
@@ -26,6 +32,12 @@ export const CommentBody = styled.div`
 `
 
 export const Upvote = styled(GoArrowUp)`
+  font-size: 24px;
 `
 export const Downvote = styled(GoArrowDown)`
+  font-size: 24px;
+`
+export const CommentHeader = styled.div`
+  display: flex;
+  align-items: center;
 `
--- client/src/Components/PostComments/PostComment.js
+++ client/src/Components/PostComments/PostComment.js
@@ -1,20 +1,37 @@
-import react from "react";
+import react, {useState} from "react";
 import {
   CommentWrapper,
   DotsGroup,
   CommentContainer,
+  CommentContent,
   Username,
   Upvotes,
   TimeAgo,
+  CommentHeader,
   CommentBody,
   Upvote,
   Downvote
   
 } from "./PostComment.components"
-const PostComment = (user, comment) => {
+const PostComment = ({user, comment}) => {
+  const [upvotes, setUpvotes] = useState(comment.upvotes.length - comment.downvotes.length)
   return (
     <CommentWrapper>
-      
+      <CommentContainer>
+        <DotsGroup>
+          <Upvote></Upvote>
+          <Downvote></Downvote>
+        </DotsGroup>
+
+        <CommentContent>
+          <CommentHeader>
+            <Username>{comment._id}</Username>&nbsp;&middot;&nbsp;
+            <Upvotes>{upvotes} points</Upvotes>&nbsp;&middot;&nbsp;
+            <TimeAgo>12h ago</TimeAgo>
+          </CommentHeader>
+        </CommentContent>
+
+      </CommentContainer>
     </CommentWrapper>
   )
 }
